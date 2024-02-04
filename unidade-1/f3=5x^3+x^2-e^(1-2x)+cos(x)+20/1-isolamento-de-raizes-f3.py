56f151cc8d0e70773e4d7e87bb4f7652e747c58b
Daniel Vitor da Silva <yodaniel.vitor1@hotmail.com>
2024-02-03 21:53:46 -0300

    Resolução da função f3=5x^3+x^2-e^(1-2x)+cos(x)+20

---
+++ unidade-1/f3=5x^3+x^2-e^(1-2x)+cos(x)+20/1-isolamento-de-raizes-f3.py
@@ -0,0 +1,96 @@
+# Implementação de Função para o Isolamento de Raízes
+from prettytable import PrettyTable
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import os
+from datetime import datetime
+
+# Variável para determinar qual vai ser a função.
+nomeDaFuncao = 'f(x)=5x^3+x^2-e^(1-2x)+cos(x)+20'
+
+diretorio_resultados = "unidade-1/f3=5x^3+x^2-e^(1-2x)+cos(x)+20/resultados-em-txt-e-grafico-f3"
+
+# Criar a estrutura de diretório, se não existir
+if not os.path.exists(diretorio_resultados):
+    os.makedirs(diretorio_resultados)
+
+# Obter a data e hora atual
+data_e_hora_atual = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+
+# Definir o caminho completo para os arquivos
+caminho_tabela = os.path.join(
+    diretorio_resultados, f'1-isolamento-de-raizes-{nomeDaFuncao}-{data_e_hora_atual}.txt')
+caminho_grafico = os.path.join(
+    diretorio_resultados, f'1-isolamento-de-raizes-{nomeDaFuncao}-grafico-{data_e_hora_atual}.jpg')
+
+print(f'\nIsolamento das raízes da função {nomeDaFuncao}')
+# a = Início do intervalo.
+a = -5
+# b = Final do intervalo.
+b = 5
+# h = Amplitude do intervalo.
+amplitude = 1
+print(f'I=[{a},{b}] e Amplitude={amplitude}.\n')
+
+# Medindo o tempo de início do cálculo
+inicioTempo = time.time()
+
+
+# Função para isolamento das Raízes:
+def f(x):
+    return 5 * x**3 + x**2 - np.exp(1-2*x) + np.cos(x) + 20
+
+
+# Valores de x:
+valoresDeX = np.arange(a, b + amplitude, amplitude)
+
+# Valores de y:
+valoresDeY = [f(x) for x in valoresDeX]
+
+# Criando a tabela com PrettyTable
+tabelaResultados = PrettyTable()
+tabelaResultados.field_names = ['x', 'f(x)']
+
+for x, y in zip(valoresDeX, valoresDeY):
+    tabelaResultados.add_row([x, y])
+
+# Imprimir a tabela no console
+print(tabelaResultados)
+
+# Salvando a tabela em um arquivo txt com informações adicionais
+with open(caminho_tabela, 'w') as file:
+    file.write(f'Isolamento das raízes da função {nomeDaFuncao}\n')
+    file.write(
+        f'I=[{a},{b}] e Amplitude={amplitude}.\n')
+    file.write(str(tabelaResultados))
+
+
+# Medindo o tempo de fim do cálculo
+fimTempo = time.time()
+# Calculando e mostrando o tempo total de cálculo
+tempoTotal = fimTempo - inicioTempo
+print(f'\nO tempo de execução foi de {tempoTotal:.6f} segundos.')
+
+# Adicionar o tempo de execução ao arquivo de texto
+with open(caminho_tabela, 'a') as file:
+    file.write(f'\nO tempo de execução foi de {tempoTotal:.6f} segundos.\n')
+
+# Criação do gráfico:
+x = np.linspace(a, b, 100)
+y = f(x)
+
+plt.plot(x, y)
+plt.scatter(valoresDeX, valoresDeY, color='red',
+            label='Pontos para isolamento de raízes')
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.title(f'Função {nomeDaFuncao} no I=[{a}, {b}] e h={amplitude}')
+plt.legend()
+plt.grid(True)
+
+# Salvar o gráfico em um arquivo jpg
+plt.savefig(caminho_grafico)
+
+# Exibir o gráfico na tela
+plt.show()
