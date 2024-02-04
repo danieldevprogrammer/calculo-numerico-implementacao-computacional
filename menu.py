a68c47c8b0c0fedc7044092c9b75c6a78291288d
Daniel Vitor da Silva <yodaniel.vitor1@hotmail.com>
2024-02-03 23:03:25 -0300

    Menu da primeira unidade completo, finalizado e feito algumas melhorias na organização.

--- menu.py
+++ menu.py
@@ -59,6 +59,7 @@ while True:
             if escolhaDaFuncao == 0:
                 break  # Voltar para o menu anterior
 
+            # f0 = x^3 - 9x + 5
             if escolhaDaFuncao == 1:
                 print('\nA função Escolhida foi: f0 = x^3 - 9x + 5')
 
@@ -133,11 +134,339 @@ while True:
                     else:
                         print(Fore.RED + 'Escolha inválida!\n' + Style.RESET_ALL)
 
-            # elif escolhaDa Funcao == 2:
+            # f1 = 2x^4 + 4x^3 + 3x^2 - 10x - 15
+            if escolhaDaFuncao == 2:
+                print('\nA função Escolhida foi: f1 = 2x^4 + 4x^3 + 3x^2 - 10x - 15')
+
+                while True:
+                    print('Escolha o método que deseja executar:')
+                    print(Fore.GREEN + '1. Isolamento de Raízes' + Style.RESET_ALL)
+                    print(Fore.GREEN + '2. Método da Bissecção')
+                    print(Fore.GREEN + '3. Método da Falsa Posição' +
+                          Style.RESET_ALL)
+                    print(Fore.GREEN + '4. Método do Ponto Fixo' + Style.RESET_ALL)
+                    print(Fore.GREEN + '5. Método de Newton')
+                    print(Fore.GREEN + '6. Método da Secante')
+                    print(Fore.YELLOW +
+                          '0. Voltar para o menu anterior' + Style.RESET_ALL)
+
+                    try:
+                        escolhaDoMetodo = int(
+                            input('Digite o número do método que deseja executar ou 0 para voltar: '))
+                    except ValueError:
+                        print(
+                            Fore.RED + 'Por favor, insira um número válido.\n' + Style.RESET_ALL)
+                        continue
+
+                    script_directory = os.path.dirname(
+                        os.path.realpath(__file__))
+
+                    if escolhaDoMetodo == 1:
+                        arquivo = os.path.join(
+                            script_directory, "unidade-1/f1=2x^4+4x^3+3x^2-10x-15/1-isolamento-de-raizes-f1.py")
+                        comando = f"python3 '{arquivo}'"
+                        subprocess.run(comando, shell=True)
+                        print('Finalizado.\n')
+
+                    elif escolhaDoMetodo == 2:
+                        arquivo = os.path.join(
+                            script_directory, "unidade-1/f1=2x^4+4x^3+3x^2-10x-15/2-metodo-da-bisseccao-f1.py")
+                        comando = f"python3 '{arquivo}'"
+                        subprocess.run(comando, shell=True)
+                        print('Finalizado.\n')
+
+                    elif escolhaDoMetodo == 3:
+                        arquivo = os.path.join(
+                            script_directory, "unidade-1/f1=2x^4+4x^3+3x^2-10x-15/3-metodo-da-falsa-posicao-f1.py")
+                        comando = f"python3 '{arquivo}'"
+                        subprocess.run(comando, shell=True)
+                        print('Finalizado.\n')
+
+                    elif escolhaDoMetodo == 4:
+                        arquivo = os.path.join(
+                            script_directory, "unidade-1/f1=2x^4+4x^3+3x^2-10x-15/4-metodo-do-ponto-fixo-f1.py")
+                        comando = f"python3 '{arquivo}'"
+                        subprocess.run(comando, shell=True)
+                        print('Finalizado.\n')
+
+                    elif escolhaDoMetodo == 5:
+                        arquivo = os.path.join(
+                            script_directory, "unidade-1/f1=2x^4+4x^3+3x^2-10x-15/5-metodo-de-newton-f1.py")
+                        comando = f"python3 '{arquivo}'"
+                        subprocess.run(comando, shell=True)
+                        print('Finalizado.\n')
+
+                    elif escolhaDoMetodo == 6:
+                        arquivo = os.path.join(
+                            script_directory, "unidade-1/f1=2x^4+4x^3+3x^2-10x-15/6-metodo-da-secante-f1.py")
+                        comando = f"python3 '{arquivo}'"
+                        subprocess.run(comando, shell=True)
+                        print('Finalizado.\n')
+
+                    elif escolhaDoMetodo == 0:
+                        break  # Voltar para o menu anterior
+
+                    else:
+                        print(Fore.RED + 'Escolha inválida!\n' + Style.RESET_ALL)
+
+            # f2 = x^5 - 2x^4 - 9x^3 + 22x^2 + 4x - 24
+            if escolhaDaFuncao == 3:
+                print(
+                    '\nA função Escolhida foi: f2 = x^5 - 2x^4 - 9x^3 + 22x^2 + 4x - 24')
+
+                while True:
+                    print('Escolha o método que deseja executar:')
+                    print(Fore.GREEN + '1. Isolamento de Raízes' + Style.RESET_ALL)
+                    print(Fore.GREEN + '2. Método da Bissecção')
+                    print(Fore.GREEN + '3. Método da Falsa Posição' +
+                          Style.RESET_ALL)
+                    print(Fore.GREEN + '4. Método do Ponto Fixo' + Style.RESET_ALL)
+                    print(Fore.GREEN + '5. Método de Newton')
+                    print(Fore.GREEN + '6. Método da Secante')
+                    print(Fore.YELLOW +
+                          '0. Voltar para o menu anterior' + Style.RESET_ALL)
+
+                    try:
+                        escolhaDoMetodo = int(
+                            input('Digite o número do método que deseja executar ou 0 para voltar: '))
+                    except ValueError:
+                        print(
+                            Fore.RED + 'Por favor, insira um número válido.\n' + Style.RESET_ALL)
+                        continue
+
+                    script_directory = os.path.dirname(
+                        os.path.realpath(__file__))
+
+                    if escolhaDoMetodo == 1:
+                        arquivo = os.path.join(
+                            script_directory, "unidade-1/f2=x^5-2x^4-9x^3+22x^2+4x-24/1-isolamento-de-raizes-f2.py")
+                        comando = f"python3 '{arquivo}'"
+                        subprocess.run(comando, shell=True)
+                        print('Finalizado.\n')
+
+                    elif escolhaDoMetodo == 2:
+                        arquivo = os.path.join(
+                            script_directory, "unidade-1/f2=x^5-2x^4-9x^3+22x^2+4x-24/2-metodo-da-bisseccao-f2.py")
+                        comando = f"python3 '{arquivo}'"
+                        subprocess.run(comando, shell=True)
+                        print('Finalizado.\n')
+
+                    elif escolhaDoMetodo == 3:
+                        arquivo = os.path.join(
+                            script_directory, "unidade-1/f2=x^5-2x^4-9x^3+22x^2+4x-24/3-metodo-da-falsa-posicao-f2.py")
+                        comando = f"python3 '{arquivo}'"
+                        subprocess.run(comando, shell=True)
+                        print('Finalizado.\n')
+
+                    elif escolhaDoMetodo == 4:
+                        arquivo = os.path.join(
+                            script_directory, "unidade-1/f2=x^5-2x^4-9x^3+22x^2+4x-24/4-metodo-do-ponto-fixo-f2.py")
+                        comando = f"python3 '{arquivo}'"
+                        subprocess.run(comando, shell=True)
+                        print('Finalizado.\n')
+
+                    elif escolhaDoMetodo == 5:
+                        arquivo = os.path.join(
+                            script_directory, "unidade-1/f2=x^5-2x^4-9x^3+22x^2+4x-24/5-metodo-de-newton-f2.py")
+                        comando = f"python3 '{arquivo}'"
+                        subprocess.run(comando, shell=True)
+                        print('Finalizado.\n')
+
+                    elif escolhaDoMetodo == 6:
+                        arquivo = os.path.join(
+                            script_directory, "unidade-1/f2=x^5-2x^4-9x^3+22x^2+4x-24/6-metodo-da-secante-f2.py")
+                        comando = f"python3 '{arquivo}'"
+                        subprocess.run(comando, shell=True)
+                        print('Finalizado.\n')
+
+                    elif escolhaDoMetodo == 0:
+                        break  # Voltar para o menu anterior
+
+                    else:
+                        print(Fore.RED + 'Escolha inválida!\n' + Style.RESET_ALL)
+
+            # f3 = 5x^3 + x^2 - e^(1-2x) + cos(x) + 20
+            if escolhaDaFuncao == 4:
+                print(
+                    '\nA função Escolhida foi: f3 = 5x^3 + x^2 - e^(1-2x) + cos(x) + 20')
+
+                while True:
+                    print('Escolha o método que deseja executar:')
+                    print(Fore.GREEN + '1. Isolamento de Raízes' + Style.RESET_ALL)
+                    print(Fore.GREEN + '2. Método da Bissecção')
+                    print(Fore.GREEN + '3. Método da Falsa Posição' +
+                          Style.RESET_ALL)
+                    print(Fore.GREEN + '4. Método do Ponto Fixo' + Style.RESET_ALL)
+                    print(Fore.GREEN + '5. Método de Newton')
+                    print(Fore.GREEN + '6. Método da Secante')
+                    print(Fore.YELLOW +
+                          '0. Voltar para o menu anterior' + Style.RESET_ALL)
+
+                    try:
+                        escolhaDoMetodo = int(
+                            input('Digite o número do método que deseja executar ou 0 para voltar: '))
+                    except ValueError:
+                        print(
+                            Fore.RED + 'Por favor, insira um número válido.\n' + Style.RESET_ALL)
+                        continue
+
+                    script_directory = os.path.dirname(
+                        os.path.realpath(__file__))
+
+                    if escolhaDoMetodo == 1:
+                        arquivo = os.path.join(
+                            script_directory, "unidade-1/f3=5x^3+x^2-e^(1-2x)+cos(x)+20/1-isolamento-de-raizes-f3.py")
+                        comando = f"python3 '{arquivo}'"
+                        subprocess.run(comando, shell=True)
+                        print('Finalizado.\n')
+
+                    elif escolhaDoMetodo == 2:
+                        arquivo = os.path.join(
+                            script_directory, "unidade-1/f3=5x^3+x^2-e^(1-2x)+cos(x)+20/2-metodo-da-bisseccao-f3.py")
+                        comando = f"python3 '{arquivo}'"
+                        subprocess.run(comando, shell=True)
+                        print('Finalizado.\n')
+
+                    elif escolhaDoMetodo == 3:
+                        arquivo = os.path.join(
+                            script_directory, "unidade-1/f3=5x^3+x^2-e^(1-2x)+cos(x)+20/3-metodo-da-falsa-posicao-f3.py")
+                        comando = f"python3 '{arquivo}'"
+                        subprocess.run(comando, shell=True)
+                        print('Finalizado.\n')
+
+                    elif escolhaDoMetodo == 4:
+                        arquivo = os.path.join(
+                            script_directory, "unidade-1/f3=5x^3+x^2-e^(1-2x)+cos(x)+20/4-metodo-do-ponto-fixo-f3.py")
+                        comando = f"python3 '{arquivo}'"
+                        subprocess.run(comando, shell=True)
+                        print('Finalizado.\n')
+
+                    elif escolhaDoMetodo == 5:
+                        arquivo = os.path.join(
+                            script_directory, "unidade-1/f3=5x^3+x^2-e^(1-2x)+cos(x)+20/5-metodo-de-newton-f3.py")
+                        comando = f"python3 '{arquivo}'"
+                        subprocess.run(comando, shell=True)
+                        print('Finalizado.\n')
+
+                    elif escolhaDoMetodo == 6:
+                        arquivo = os.path.join(
+                            script_directory, "unidade-1/f3=5x^3+x^2-e^(1-2x)+cos(x)+20/6-metodo-da-secante-f3.py")
+                        comando = f"python3 '{arquivo}'"
+                        subprocess.run(comando, shell=True)
+                        print('Finalizado.\n')
+
+                    elif escolhaDoMetodo == 0:
+                        break  # Voltar para o menu anterior
+
+                    # f3 = 5x^3 + x^2 - e^(1-2x) + cos(x) + 20
+
+            # f4 = sen(x)x + 4
+            if escolhaDaFuncao == 5:
+                print(
+                    '\nA função Escolhida foi:  f 4=sen(x ) x+4')
+
+                while True:
+                    print('Escolha o método que deseja executar:')
+                    print(Fore.GREEN + '1. Isolamento de Raízes' + Style.RESET_ALL)
+                    print(Fore.GREEN + '2. Método da Bissecção')
+                    print(Fore.GREEN + '3. Método da Falsa Posição' +
+                          Style.RESET_ALL)
+                    print(Fore.GREEN + '4. Método do Ponto Fixo' + Style.RESET_ALL)
+                    print(Fore.GREEN + '5. Método de Newton')
+                    print(Fore.GREEN + '6. Método da Secante')
+                    print(Fore.YELLOW +
+                          '0. Voltar para o menu anterior' + Style.RESET_ALL)
+
+                    try:
+                        escolhaDoMetodo = int(
+                            input('Digite o número do método que deseja executar ou 0 para voltar: '))
+                    except ValueError:
+                        print(
+                            Fore.RED + 'Por favor, insira um número válido.\n' + Style.RESET_ALL)
+                        continue
+
+                    script_directory = os.path.dirname(
+                        os.path.realpath(__file__))
+
+                    if escolhaDoMetodo == 1:
+                        arquivo = os.path.join(
+                            script_directory, "unidade-1/f4=sen(x)x+4/1-isolamento-de-raizes-f4.py")
+                        comando = f"python3 '{arquivo}'"
+                        subprocess.run(comando, shell=True)
+                        print('Finalizado.\n')
+
+                    elif escolhaDoMetodo == 2:
+                        arquivo = os.path.join(
+                            script_directory, "unidade-1/f4=sen(x)x+4/2-metodo-da-bisseccao-f4.py")
+                        comando = f"python3 '{arquivo}'"
+                        subprocess.run(comando, shell=True)
+                        print('Finalizado.\n')
+
+                    elif escolhaDoMetodo == 3:
+                        arquivo = os.path.join(
+                            script_directory, "unidade-1/f4=sen(x)x+4/3-metodo-da-falsa-posicao-f4.py")
+                        comando = f"python3 '{arquivo}'"
+                        subprocess.run(comando, shell=True)
+                        print('Finalizado.\n')
+
+                    elif escolhaDoMetodo == 4:
+                        arquivo = os.path.join(
+                            script_directory, "unidade-1/f4=sen(x)x+4/4-metodo-do-ponto-fixo-f4.py")
+                        comando = f"python3 '{arquivo}'"
+                        subprocess.run(comando, shell=True)
+                        print('Finalizado.\n')
+
+                    elif escolhaDoMetodo == 5:
+                        arquivo = os.path.join(
+                            script_directory, "unidade-1/f4=sen(x)x+4/5-metodo-de-newton-f4.py")
+                        comando = f"python3 '{arquivo}'"
+                        subprocess.run(comando, shell=True)
+                        print('Finalizado.\n')
+
+                    elif escolhaDoMetodo == 6:
+                        arquivo = os.path.join(
+                            script_directory, "unidade-1/f4=sen(x)x+4/6-metodo-da-secante-f4.py")
+                        comando = f"python3 '{arquivo}'"
+                        subprocess.run(comando, shell=True)
+                        print('Finalizado.\n')
+
+                    elif escolhaDoMetodo == 0:
+                        break  # Voltar para o menu anterior
 
             else:
                 print(Fore.RED + 'Função inválida!\n' + Style.RESET_ALL)
-    # elif unidade == 1:
+
+    elif unidade == 2:
+        print(Fore.RED + '\nSegunda Unidade em construção!' +
+              Fore.YELLOW + '\nAGUARDE...' + Style.RESET_ALL)
+
+        usuarioDigitou = -1  # Inicializar para um valor diferente de 0
+
+        while usuarioDigitou != 0:
+            try:
+                usuarioDigitou = int(
+                    input('\nDigite 0 para voltar para o menu principal: '))
+            except ValueError:
+                print(
+                    Fore.RED + 'Por favor, insira um número válido.\n' + Style.RESET_ALL)
+
+        # Se o usuárioDigitou for 0, o código continuará para o próximo loop
+
+    elif unidade == 3:
+        print(Fore.RED + '\nTerceira Unidade em construção!' +
+              Fore.YELLOW + '\nAGUARDE...' + Style.RESET_ALL)
+
+        usuarioDigitou = -1  # Inicializar para um valor diferente de 0
+
+        while usuarioDigitou != 0:
+            try:
+                usuarioDigitou = int(
+                    input('\nDigite 0 para voltar para o menu principal: '))
+            except ValueError:
+                print(
+                    Fore.RED + 'Por favor, insira um número válido.\n' + Style.RESET_ALL)
+
+        # Se o usuárioDigitou for 0, o código continuará para o próximo loop
 
     else:
         print(Fore.RED + 'Escolha de unidade inválida!\n' + Style.RESET_ALL)
